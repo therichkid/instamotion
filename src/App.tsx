9976f45696042bea1468579863476362d4780124
Richard Zille <therichkid@gmx.de>
2021-11-23 19:03:56 +0100

    Implement simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.scss';
 import Details from './components/details/Details';
 import Overview from './components/overview/Overview';
 
 function App() {
   return (
-    <div>
-      <Overview />
-      <Details />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/details">Details</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Overview />}></Route>
+          <Route path="/details" element={<Details />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
