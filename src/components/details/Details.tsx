ea22f4e549ce557fa9c0a0f47ef45a6929a46add
Richard Zille <therichkid@gmx.de>
2021-11-23 18:44:07 +0100

    Add router and sass

---
+++ src/components/details/Details.tsx
@@ -0,0 +1,11 @@
+import './Details.scss';
+
+function Details() {
+  return (
+    <div className="details">
+      <h2>Details</h2>
+    </div>
+  );
+}
+
+export default Details;
---
+++ src/components/overview/Overview.tsx
@@ -0,0 +1,11 @@
+import './Overview.scss';
+
+function Overview() {
+  return (
+    <div className="overview">
+      <h2>Overview</h2>
+    </div>
+  );
+}
+
+export default Overview;
